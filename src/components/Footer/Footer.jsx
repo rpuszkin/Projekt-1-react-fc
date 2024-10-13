985916a1bd15f370ce5eafa9483c44d95688036a
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-10-13 19:54:47 +0200

    react fragment in Footer.jsx was deleted

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,35 +7,25 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <footer>
-        <div className="container">
-          <p className=" font-bold font-white ">
-            Wszelkie prawa zastrzeżone ® 2023 moja firma.
-          </p>
+    <footer>
+      <div className="container">
+        <p className=" font-bold font-white ">
+          Wszelkie prawa zastrzeżone ® 2023 moja firma.
+        </p>
 
-          <div className="footer-icn">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.instagram.com/"
-            >
-              <FontAwesomeIcon icon={faInstagram} className="footer-icn" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.facebook.com/"
-            >
-              <FontAwesomeIcon
-                icon={faFacebookSquare}
-                className="fa-square-facebook footer-icn"
-              />
-            </a>
-          </div>
+        <div className="footer-icn">
+          <a target="_blank" rel="noreferrer" href="https://www.instagram.com/">
+            <FontAwesomeIcon icon={faInstagram} className="footer-icn" />
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.facebook.com/">
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              className="fa-square-facebook footer-icn"
+            />
+          </a>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 }
 export default Footer;
